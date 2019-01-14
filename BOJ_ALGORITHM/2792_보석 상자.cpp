eb2a4831908d489f580405e85ae8385d22c98085
genie97 <qndnd1@naver.com>
2019-01-15 08:20:28 +0900

    BOJ2792 보석 상자

---
+++ BOJ_ALGORITHM/2792_보석 상자.cpp
@@ -0,0 +1,30 @@
+/*
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int INF = 987654321;
+int N, M, jew[300002];
+int ok(int mid) {
+	int sum = 0;
+	for (int i = 1; i<=M;	i++) {
+		sum += (jew[i]-1)/ mid+1;
+	}
+	if (sum <= N)
+		return 1;
+	return 0;
+}
+int main(){
+	scanf("%d %d", &N, &M);
+	for (int i = 1; i<=M; i++)
+		scanf("%d", &jew[i]);
+	int left = 0, right= INF;
+	while (left<right) {
+		int mid = (left + right) / 2;
+		if (ok(mid))
+			right = mid;
+		else 
+			left = mid + 1;
+	}
+	printf("%d\n", left);
+}*/
+

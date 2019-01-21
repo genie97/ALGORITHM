7aca6090d2b72d17d06b07b39da363111989050d
genie97 <qndnd1@naver.com>
2019-01-21 17:05:23 +0900

    BOJ11718 그대로 출력하기

--- Python/11718_그대로 출력하기.py
+++ Python/11718_그대로 출력하기.py
@@ -1,11 +1,5 @@
-list = []
-n=0
-while n<3:
-    list.append(input(""))
-    n+=1
-n=0
-while n<3:
-    print(list[n])
-    n+=1
-
-
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break

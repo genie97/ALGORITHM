2037725bcf988d8e914c6e0ddf84ad307f4e46ba
genie97 <qndnd1@naver.com>
2019-02-15 09:28:58 +0900

    BOJ1261 알고스팟

--- BOJ_ALGORITHM/1261_알고스팟 .cpp
+++ BOJ_ALGORITHM/1261_알고스팟 .cpp
@@ -1,9 +1,8 @@
 #include<cstdio>
-#include<vector>
+#include<cstring>
 #include<queue>
 using namespace std;
 
-#define INF 987654321
 priority_queue<pair<int, pair<int, int>>> pq; //first: weight second.first : xรเ second.second:yรเ 
 
 int N, M;
@@ -14,31 +13,35 @@ int moveY[4] = { -1,1,0,0 };
 
 int main() {
 	scanf("%d %d", &N, &M);
+	memset(weight, -1, sizeof(weight));
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
 			scanf("%1d", &maze[i][j]);
-			weight[i][j] = INF;
 		}
 	}
-	weight[0][0] = 0;
 	pq.push({ 0,{ 0,0 } });
 
 	while (!pq.empty()) {
 		int x = pq.top().second.first;
 		int y = pq.top().second.second;
-		int w = pq.top().first;
+		int w = -pq.top().first;
 		pq.pop();
-		if (w > weight[x][y])
+		if (weight[x][y] != -1)
 			continue;
+
+		weight[x][y] = w;
+
 		for (int i = 0; i < 4; i++) {
 			int nextX = x + moveX[i];
 			int nextY = y + moveY[i];
-			if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N)
-				continue;
-			int nextW = w + maze[nextX][nextY];
-			if (nextW < weight[nextX][nextY]) {
-				weight[nextX][nextY] = nextW;
-				pq.push({ nextW,{ nextX,nextY } });
+			if (nextX >= 0 && nextX < M && nextY >= 0 && nextY < N) {
+				if (weight[nextX][nextY] == -1) {
+					if (maze[nextX][nextY] == 1) {
+						pq.push({ -(w + 1),{ nextX,nextY } });
+					}
+					else
+						pq.push({ -w,{ nextX,nextY } });
+				}
 			}
 		}
 	}

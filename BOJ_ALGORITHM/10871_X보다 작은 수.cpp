10aa18a89fdf3cf99d4146cd4d38d6423f7ece8b
genie97 <qndnd1@naver.com>
2019-03-26 01:17:52 +0900

    BOJ10871 X보다 작은 수

---
+++ BOJ_ALGORITHM/10871_X보다 작은 수.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+int X, N, A[10000], ans[10000];
+int main() {
+	scanf("%d%d", &N, &X);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+	int j = 0;
+	for (int i = 0; i < N; i++) {
+		if (A[i] < 5) {
+			ans[j] = A[i];
+			j++;
+		}
+	}
+	for (int i = 0; i < j; i++) {
+		printf("%d ", ans[i]);
+	}
+}

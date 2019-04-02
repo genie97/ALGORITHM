a0523f96be98e1972028b8f040ffd329e52ac0c7
Genie <32705138+genie97@users.noreply.github.com>
2019-04-03 02:49:55 +0900

    BOJ2869 달팽이는 올라가고 싶다_수학

--- BOJ_ALGORITHM/2869_달팽이는 올라가고 싶다_수학.cpp
+++ BOJ_ALGORITHM/2869_달팽이는 올라가고 싶다_수학.cpp
@@ -1,8 +1,8 @@
-/*
-#include<stdio.h>
+#include<cstdio>
+#include<cmath>
 int main() {
 	int A, B, V;
-	scanf("%d %d %d", &A, &B, &V);
-	printf("%d", ((V - A - 1) / (A - B)) + 2);
+	scanf("%d%d%d", &A, &B, &V);
+	int ans = ceil((double)(V - A) / (double)(A - B)) + 1;
+	printf("%d\n",ans);
 }
-*/

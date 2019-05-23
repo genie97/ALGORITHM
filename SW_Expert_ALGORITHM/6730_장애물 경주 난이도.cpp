d4689a4858a7fe7ea73cf36f37f18da1956f70ae
genie97 <qndnd1@naver.com>
2019-05-24 00:05:43 +0900

    SW6730 장애물 경주 난이도

---
+++ SW_Expert_ALGORITHM/6730_장애물 경주 난이도.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int T, N, cnt = 0;
+int main() {
+	scanf("%d", &T);
+	while (T--) {
+		int N;
+		int UM = 0, DM = 0, check = 0;
+		int block[101];
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			scanf("%d", &block[i]);
+		}
+		for (int i = 0; i < N - 1; i++) {
+			if (block[i] < block[i + 1]) {
+				UM = max(UM, block[i + 1] - block[i]);
+			}
+			else if (block[i] > block[i + 1]) {
+				DM = max(DM, block[i] - block[i + 1]);
+			}
+		}
+		cnt++;
+		printf("#%d %d %d\n", cnt, UM, DM);
+	}
+}

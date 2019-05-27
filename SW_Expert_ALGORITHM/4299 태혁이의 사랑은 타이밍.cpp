b95ebf0cbdbf7ef85c7743e4d4c2f3dcece32a37
genie97 <qndnd1@naver.com>
2019-05-28 01:14:03 +0900

    SW4299 태혁이의 사랑은 타이밍

--- SW_Expert_ALGORITHM/4299 태혁이의 사랑은 타이밍.cpp
+++ SW_Expert_ALGORITHM/4299 태혁이의 사랑은 타이밍.cpp
@@ -1,12 +1,11 @@
 #include<cstdio>
 int T, D, H, M, cnt = 0;
-//기준 오전 11일 11시 11분
 int main() {
 	scanf("%d", &T);
 	while (T--) {
 		cnt++;
 		scanf("%d%d%d", &D, &H, &M);
-		int first = 11 * 24 * 60 + 11 * 60 + 11;
+		int first = 11 * 24 * 60 + 11 * 60 + 11; //기준 오전 11일 11시 11분
 		int total = D * 24 * 60 + H * 60 + M;
 		if (total- first >= 0)
 			printf("#%d %d\n", cnt, total-first);

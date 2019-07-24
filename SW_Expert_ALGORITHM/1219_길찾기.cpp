cbd77f396e4f11dd0ef140041ba4ca9b758a02b7
genie97 <qndnd1@naver.com>
2019-07-24 17:01:48 +0900

    1219 길찾기

---
+++ SW_Expert_ALGORITHM/1219_길찾기.cpp
@@ -0,0 +1,27 @@
+#include<cstdio>
+#include<cstring>
+#include<vector>
+using namespace std;
+int T, N, res;
+vector<int> vt[100];
+void dfs(int v) {
+	if (v == 99) 
+		res = 1;
+	for (int i = 0; i < vt[v].size(); i++) {
+		dfs(vt[v][i]);
+	}
+}
+int main() {
+	for (int tc = 0; tc < 10; tc++) {
+		scanf("%d%d", &T, &N);
+		memset(vt, 0, sizeof(vt));
+		res = 0;
+		for (int i = 0; i < N; i++) {
+			int a, b;
+			scanf("%d%d", &a, &b);
+			vt[a].push_back(b);
+		}
+		dfs(0);
+		printf("#%d %d\n", T, res);
+	}
+}

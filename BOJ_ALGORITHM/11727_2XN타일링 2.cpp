b6b580b0063d44ea7eb54f4418c1b36434d4dd2e
genie97 <qndnd1@naver.com>
2019-07-28 18:15:42 +0900

    BOJ11727 2XN타일링 2

--- BOJ_ALGORITHM/11727_2XN타일링 2.cpp
+++ BOJ_ALGORITHM/11727_2XN타일링 2.cpp
@@ -4,8 +4,9 @@ int main() {
 	scanf("%d", &n);
 	int dp[1002];
 	dp[0] = 0, dp[1] = 1, dp[2] = 3;
-	for (int i = 0; i < n; i++) {
-		int res = (dp[n - 1] + 2 * dp[n - 2]) % 10007;
-		printf("%d", res);
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]);
+		dp[i] = dp[i] % 10007;
 	}
+	printf("%d\n", dp[n]);
 }

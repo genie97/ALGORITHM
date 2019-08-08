95531d9033f049a1d9b36a6d26dc29e5b90814ae
Genie <qndnd1@naver.com>
2019-08-09 01:33:02 +0900

    SW3752 가능한 시험 점수

--- SW_Expert_ALGORITHM/3752_가능한 시험 점수.cpp
+++ SW_Expert_ALGORITHM/3752_가능한 시험 점수.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 int T, N;
 int main() {
-	scanf("%d", &T);
-	for (int tc = 1; tc <= T; tc++) {
-		int maxV = 0, sc = 0, res = 0, check[10000] = { 0, };
-		scanf("%d", &N);
-		check[0] = 1;
-		for (int i = 0; i < N; i++) {
-			scanf("%d", &sc);
-			maxV += sc;
-			for (int j = maxV; j >= sc; j--)
-				check[j] |= check[j - sc];
-		}
-		for (int i = 0; i <= maxV; i++) {
-			res += check[i];
-		}
-		printf("#%d %d\n", tc, res);
-	}
+    scanf("%d", &T);
+    for (int tc = 1; tc <= T; tc++) {
+        int maxV = 0, sc = 0, res = 0, check[10000] = { 0, };
+        scanf("%d", &N);
+        check[0] = 1;
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &sc);
+            maxV += sc;
+            for (int j = maxV; j >= sc; j--)
+                check[j] |= check[j - sc];
+        }
+        for (int i = 0; i <= maxV; i++) {
+            res += check[i];
+        }
+        printf("#%d %d\n", tc, res);
+    }
 }

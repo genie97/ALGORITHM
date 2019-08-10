447d74993e79f575c4e4d28bb9af79535d6cacd7
genie97 <qndnd1@naver.com>
2019-08-10 12:30:57 +0900

    11722_가장 긴 감소하는 부분 수열

---
+++ BOJ_ALGORITHM/11722_가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+int N, a[1001], dp[1001];
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = 0; i < N; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (a[i] < a[j] && dp[i] < dp[j] + 1)
+				dp[i] = dp[j]+ 1;
+		}
+	}
+	int res = *max_element(dp, dp + N);
+	printf("%d\n", res);
+}

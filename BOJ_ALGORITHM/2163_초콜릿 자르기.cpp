3a029a7d9d6f8d2d9c3ff18a5ca593143c26ad81
Genie <qndnd1@naver.com>
2019-08-13 02:12:04 +0900

    BOJ2163 초콜릿 자르기

--- BOJ_ALGORITHM/2163_초콜릿 자르기.cpp
+++ BOJ_ALGORITHM/2163_초콜릿 자르기.cpp
@@ -2,7 +2,7 @@
 int main() {
 	int N, M;
 	scanf("%d %d", &N, &M);
-	//M*N 크기일때, M*1사이즈를 자르는데 M-1번
+	// M*N 크기일때, M*1사이즈를 자르는데 M-1번
 	// M-1사이즈가 N개 있으므로 M(N-1)
 	// M - 1 + MN - M = MN-1
 	printf("%d\n", M*N - 1);

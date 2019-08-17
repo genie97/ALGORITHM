617f954d631352dfc658d7c44af8635857e28172
Genie <qndnd1@naver.com>
2019-08-18 01:51:52 +0900

    SW3499 퍼펙트 셔플

--- SW_Expert_ALGORITHM/3499_퍼펙트 셔플.cpp
+++ SW_Expert_ALGORITHM/3499_퍼펙트 셔플.cpp
@@ -11,11 +11,11 @@ int main() {
 		if (N % 2 == 0) N -= 1;
 		int i = 0, j = 0, k = 0;
 		string in, dec1[501], dec2[501], input;
-		cin >> in;
+		cin >> in; //공백을 기준으로 string을 입력받는 경우, 첫번째 입력은 공백기준으로 짤려서 in에 저장됨
 		dec1[j++] = in;
-		getline(cin, in);
-		stringstream ss(in);
-		while (ss >> input) {
+		getline(cin, in); //첫 번쨰 공백 이후의 모든 string이 in에 다시 저장됨
+		stringstream ss(in); 
+		while (ss >> input) { //stringstream인 ss가 공백 기준으로 다시 잘라서 input에 저장해줌  
 			i++;
 			if (i <= N / 2) dec1[j++] = input;
 			else dec2[k++] = input;

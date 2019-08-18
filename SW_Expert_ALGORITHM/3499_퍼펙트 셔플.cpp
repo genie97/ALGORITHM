f73dff64e950bc9a3537a8c5eb2b8d15e8c8fb8b
Genie <qndnd1@naver.com>
2019-08-19 01:35:12 +0900

    SW3499 퍼펙트 셔플

--- SW_Expert_ALGORITHM/3499_퍼펙트 셔플.cpp
+++ SW_Expert_ALGORITHM/3499_퍼펙트 셔플.cpp
@@ -30,3 +30,32 @@ int main() {
 		cout << "#" << tc << " " << res << endl;
 	}
 }
+/* 8개 테스트케이스 18ms
+#include<iostream>
+#include<string>
+using namespace std;
+int T, N;
+int main() {
+	scanf("%d", &T);
+	for (int tc = 1; tc <= T; tc++) {
+		scanf("%d", &N);
+		int mid;
+		N % 2 == 0 ? mid = N / 2 : mid = (N + 1) / 2;
+		int i = 0, j = 0, k = 0;
+		string in, dec1[501], dec2[501];
+		for (int i = 0; i < N; i++) {
+			cin >> in; //cin으로 입력받으면 자동으로 스페이스바 구분
+			if (i < mid) dec1[j++] = in;
+			else dec2[k++] = in;
+		}
+		string res = "";
+		for (int i = 0; i < mid; i++) {
+			res.append(dec1[i]);
+			res.append(" ");
+			res.append(dec2[i]);
+			if (i != mid)res.append(" ");
+		}
+		cout << "#" << tc << " " << res << endl;
+	}
+}
+*/

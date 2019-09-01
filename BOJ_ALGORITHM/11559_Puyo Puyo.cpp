7614b9ae073a03cff528e2a173512dbde291fbe7
Genie <qndnd1@naver.com>
2019-09-01 15:05:01 +0900

    BOJ11559 Puyo Puyo

--- BOJ_ALGORITHM/11559_Puyo Puyo.cpp
+++ BOJ_ALGORITHM/11559_Puyo Puyo.cpp
@@ -1,16 +1,16 @@
 #include<cstdio>
-#include<stack>
+#include<vector>
 #include<algorithm>
-#include<utility>
+#include<cstring>
 using namespace std;
 
 #define row 12
 #define col 6
 
 typedef pair <int, int> p;
-stack <p> s;
+vector<p> vt;
 char board[row][col];
-bool visited[row][col];
+int visited[row][col];
 
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
@@ -19,9 +19,10 @@ int dy[4] = { 0,0,1,-1 };
 void arrange() {
 	for (int i = 0; i < col; i++) {
 		for (int j = row - 2; j >= 0; j--) {
-			for (int k = 11; k > j; k--) {
+			for (int k = row - 1; k > j; k--) {
 				if (board[j][i] != '.' && board[k][i] == '.') {
 					swap(board[k][i], board[j][i]);
+					board[j][i] = '.';
 					break;
 				}
 			}
@@ -33,37 +34,33 @@ void dfs(int y, int x, char color) {
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
-		if (0 <= ny || ny < row || 0 <= nx || nx <= col) {
+		if (0 <= ny || ny < row || 0 <= nx || nx < col) {
 			if (!visited[ny][nx] && board[ny][nx] == color) {
-				s.push({ ny,nx });
-				visited[ny][nx] = true;
+				vt.push_back({ ny,nx });
+				visited[ny][nx] = 1;
 				dfs(ny, nx, color);
 			}
 		}
 	}
 }
 int main() {
-	for (int i = 0; i < row; i++) {
-		scanf("%s", &board[i]);
-	}
-
+	for (int i = 0; i < row; i++)
+		scanf("%s", board[i]);
 	int comb = 0;
-	
 	while (1) {
 		bool flag = false;
-		memset(visited, false, sizeof(visited));
+		memset(visited, 0, sizeof(visited));
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
 				if (!visited[i][j] && board[i][j] != '.') {
+					vt.clear();
 					dfs(i, j, board[i][j]);
-					if (s.size() >= 4) {
+					if (vt.size() >= 4) {
 						flag = true;
-						
-						while (!s.empty()) {
-							p top;
-							top = s.top();
-							board[top.first][top.second] = '.';
-							s.pop();
+						for (int k = 0; k < vt.size(); k++) {
+							int y = vt[k].first;
+							int x = vt[k].second;
+							board[y][x] = '.';
 						}
 					}
 				}
@@ -73,7 +70,7 @@ int main() {
 			break;
 		else
 			comb++;
-		arrange();
+		arrange();	
 	}
 	printf("%d\n", comb);
 }

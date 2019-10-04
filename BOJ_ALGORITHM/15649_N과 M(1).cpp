fa66d24bf5bd1552ce490ded43f6971d697712dd
genie97 <qndnd1@naver.com>
2019-10-05 02:11:35 +0900

    BOJ15649 N과 M (1)

---
+++ BOJ_ALGORITHM/15649_N과 M(1).cpp
@@ -0,0 +1,27 @@
+#include<cstdio>
+#include<vector>
+using namespace std;
+int num[9] = {0,};
+int N, M;
+vector<int> vt;
+void make_comb(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < vt.size(); i++) {
+			printf("%d ", vt[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (num[i] == 1)continue;
+		vt.push_back(i);
+		num[i] = 1;
+		make_comb(cnt + 1);
+		num[i] = 0;
+		vt.pop_back();
+	}
+}
+int main() {
+	scanf("%d %d", &N, &M);
+	make_comb(0);
+}

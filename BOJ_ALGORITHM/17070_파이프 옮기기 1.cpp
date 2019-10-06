effbe2be3f4285c78b7ef99dc7b4381e394e6919
Genie <qndnd1@naver.com>
2019-10-06 12:53:45 +0900

    BOJ17070 파이프 옮기기 1

--- BOJ_ALGORITHM/17070_파이프 옮기기 1.cpp
+++ BOJ_ALGORITHM/17070_파이프 옮기기 1.cpp
@@ -3,7 +3,7 @@ int N, cnt = 0;
 int map[17][17];
 int dx[3] = { 0,1,1 }, dy[3] = { 1,1,0 };
 int visit[17][17] = { 0, };
-void dfs(int x1, int y1, int x2, int y2, int dir) { //dir 0:���� 1:�밢�� 2:����
+void dfs(int x1, int y1, int x2, int y2, int dir) { //dir 0:가로 1:대각선 2:세로
 	if (x2 == N&&y2 == N)
 		cnt++;
 	switch (dir) {
@@ -16,12 +16,13 @@ void dfs(int x1, int y1, int x2, int y2, int dir) { //dir 0:���� 1:�밢�� 2:����
 			int nDir = i;
 			if (nx2 > N || ny2 > N)
 				continue;
-			if (i == 1 && (map[x2][y2 + 1] == 1 || map[x2 + 1][y2] == 1))
+			//대각선인 경우) x2,y2기준으로 오른쪽칸, 아래칸이 0이어야만 이동이 가능하다
+			if (i == 1 && (map[x2][y2 + 1] == 1 || map[x2 + 1][y2] == 1)) 
 				continue;
 			if (map[nx2][ny2] == 0 && !visit[nx1][ny1]) {
 				visit[nx1][ny1] = 1;
 				dfs(nx1, ny1, nx2, ny2, nDir);
-				visit[nx1][ny1] = 0;
+				visit[nx1][ny1] = 0; //다시 쇠파이프가 기존 위치로 이동할 경우 visit을 해제해야 함
 			}
 		}
 		break;
@@ -34,12 +35,13 @@ void dfs(int x1, int y1, int x2, int y2, int dir) { //dir 0:���� 1:�밢�� 2:����
 			int nDir = i;
 			if (nx2 > N || ny2 > N)
 				continue;
+			//대각선인 경우) x2,y2기준으로 오른쪽칸, 아래칸이 0이어야만 이동이 가능하다
 			if (i == 1 && (map[x2][y2 + 1] == 1 || map[x2 + 1][y2] == 1))
 				continue;
 			if (map[nx2][ny2] == 0 && !visit[nx1][ny1]) {
 				visit[nx1][ny1] = 1;
 				dfs(nx1, ny1, nx2, ny2, nDir);
-				visit[nx1][ny1] = 0;
+				visit[nx1][ny1] = 0; //다시 쇠파이프가 기존 위치로 이동할 경우 visit을 해제해야 함
 			}
 		}
 		break;
@@ -52,12 +54,13 @@ void dfs(int x1, int y1, int x2, int y2, int dir) { //dir 0:���� 1:�밢�� 2:����
 			int nDir = i;
 			if (nx2 > N || ny2 > N)
 				continue;
+			//대각선인 경우) x2,y2기준으로 오른쪽칸, 아래칸이 0이어야만 이동이 가능하다
 			if (i == 1 && (map[x2][y2 + 1] == 1 || map[x2 + 1][y2] == 1))
 				continue;
 			if (map[nx2][ny2] == 0 && !visit[nx1][ny1]) {
 				visit[nx1][ny1] = 1;
 				dfs(nx1, ny1, nx2, ny2, nDir);
-				visit[nx1][ny1] = 0;
+				visit[nx1][ny1] = 0; //다시 쇠파이프가 기존 위치로 이동할 경우 visit을 해제해야 함
 			}
 		}
 		break;
@@ -70,6 +73,6 @@ int main() {
 			scanf("%d", &map[i][j]);
 		}
 	}
-	dfs(1, 1, 1, 2, 0); // ���� �������� (1,1) / (1,2)
+	dfs(1, 1, 1, 2, 0); // 시작 파이프가 (1,1)/(1,2)
 	printf("%d", cnt);
 }

d3b15342a837b95f4d5b3355b02e12fb6f4314d5
Genie <qndnd1@naver.com>
2019-10-19 23:03:06 +0900

    BOJ15686 치킨배달

--- BOJ_ALGORITHM/15686_치킨배달.cpp
+++ BOJ_ALGORITHM/15686_치킨배달.cpp
@@ -1,3 +1,59 @@
+// 2019/10/19 기존 8ms->4ms
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+typedef struct Pos {
+	int x, y;
+};
+int N, M;
+int map[55][55];
+int num[15];
+int ans = 987654321;
+vector<Pos> vt;
+vector<Pos> home, chicken;
+void make_comb(int idx, int cnt) {
+	if (cnt == M) {
+		int sum = 0;
+		for (int i = 0; i < home.size(); i++) {
+			int minV = 987654321;
+			for (int j = 0; j < chicken.size(); j++) {
+				int dx = home[i].x - chicken[j].x;
+				int dy = home[i].y - chicken[j].y;
+				int dist = abs(dx) + abs(dy);
+				minV = min(dist, minV);
+			}
+			sum += minV;
+		}
+		ans = min(sum, ans);
+		return;
+	}
+	for (int i = idx; i < vt.size(); i++) {
+		if (num[i] == 1) continue;
+		num[i] = 1;
+		chicken.push_back(vt[i]);
+		make_comb(i, cnt + 1);
+		num[i] = 0;
+		chicken.pop_back();
+	}
+}
+int main() {
+	scanf("%d%d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &map[i][j]);
+			if (map[i][j] == 2) {
+				vt.push_back({ i,j });
+			}
+			if (map[i][j] == 1) {
+				home.push_back({ i,j });
+			}
+		}
+	}
+	make_comb(0, 0);
+	printf("%d", ans);
+}
+/*
 #include<cstdio>
 #include<algorithm>
 #include<vector>
@@ -43,3 +99,4 @@ int main() {
 	dfs(0, 0);
 	printf("%d\n", ans);
 }
+*/

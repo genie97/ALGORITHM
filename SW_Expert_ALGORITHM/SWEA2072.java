6abdcddecc88da1f568ffb46ef7c0bf8edc68761
genie97 <qndnd1@naver.com>
2020-01-20 23:41:26 +0900

    SW2072 홀수만 더하기

---
+++ SW_Expert_ALGORITHM/SWEA2072.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SWEA2072 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int testCase = 1; testCase <= TC; testCase++) {
+			int sum = 0;
+			for(int i=0;i<10;i++) {
+				int num = sc.nextInt();
+				if(num%2 == 1) {
+					sum = sum + num;
+				}
+			}
+			System.out.println("#" + testCase + " " + sum);
+		}
+	} // end of main
+}

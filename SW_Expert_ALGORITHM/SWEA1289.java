485afc9a4682dc602ceb86e5f59f600bf1ad73a0
genie97 <qndnd1@naver.com>
2020-01-20 23:48:52 +0900

    SW1289 원재의 메모리 복구하기

---
+++ SW_Expert_ALGORITHM/SWEA1289.java
@@ -0,0 +1,22 @@
+/**
+ * 달라지는 부분을 체크할 것!
+ * */
+import java.util.Scanner;
+public class SWEA1289{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int testCase = 1; testCase <= TC; testCase++) {	
+			char arr[] = sc.next().toCharArray();
+			int cnt = 0;
+			char cur = '0';
+			for (int len = 0; len < arr.length; len++) {	
+				if(arr[len]!=cur) {
+					cnt++;
+					cur = arr[len];
+				}
+			}
+			System.out.println("#" + testCase + " " + cnt);
+		}
+	}
+}

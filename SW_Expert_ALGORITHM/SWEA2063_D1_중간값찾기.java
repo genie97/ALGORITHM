ffb5d0833c5464eca53ec9d4cc5475e47d6528ca
Genie <qndnd1@naver.com>
2020-01-21 08:58:19 +0900

    SW2063 중간값 찾기

--- SW_Expert_ALGORITHM/SWEA2063_D1_중간값찾기.java
+++ SW_Expert_ALGORITHM/SWEA2063_D1_중간값찾기.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class SWEA2063{
+public class SWEA2063_D1_중간값찾기{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();

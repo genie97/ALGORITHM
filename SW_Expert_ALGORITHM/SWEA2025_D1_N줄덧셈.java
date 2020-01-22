d1e23d6f82102b55b36edb3086c3261c5790d569
Genie <qndnd1@naver.com>
2020-01-22 12:59:09 +0900

    SW2025 N줄덧셈

---
+++ SW_Expert_ALGORITHM/SWEA2025_D1_N줄덧셈.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SWEA2025_D1_N줄덧셈 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num*(num+1)/2);
+	}
+}

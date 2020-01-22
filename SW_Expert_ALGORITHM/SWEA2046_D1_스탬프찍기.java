948e7e08ab9be43c031c89bd124a73e22b574d47
Genie <qndnd1@naver.com>
2020-01-22 11:11:20 +0900

    SW2046 스탬프 찍기

---
+++ SW_Expert_ALGORITHM/SWEA2046_D1_스탬프찍기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SWEA2046_D1_스탬프찍기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			System.out.print("#");
+		}
+	}
+}

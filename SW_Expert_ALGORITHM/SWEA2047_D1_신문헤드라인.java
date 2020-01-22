9c1cdb1d17a04a3efa6650e1aa2273e69bb0791c
Genie <qndnd1@naver.com>
2020-01-22 11:19:39 +0900

    SWEA2047 신문 헤드라인

--- SW_Expert_ALGORITHM/SWEA2047_D1_신문헤드라인.java
+++ SW_Expert_ALGORITHM/SWEA2047_D1_신문헤드라인.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SWEA_2047_D1_½Å¹®Çìµå¶óÀÎ {
+public class SWEA_2047_D1_신문헤드라인 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String line = sc.next();

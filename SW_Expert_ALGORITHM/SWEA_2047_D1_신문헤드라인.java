bc55d2bce5e943102abf5a591a5ee176aa9b7555
Hyojin Lim <qndnd1@naver.com>
2020-01-22 10:21:46 +0900

    SW2047 SW2047 신문 헤드라인

---
+++ SW_Expert_ALGORITHM/SWEA_2047_D1_신문헤드라인.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SWEA_2047_D1_신문헤드라인 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.next();
+		System.out.println(line.toUpperCase());
+	}
+}

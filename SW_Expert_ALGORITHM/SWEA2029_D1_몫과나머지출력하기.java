6d0e85f82b6ff8f7f72b3e9e54e71244e7faf976
Genie <qndnd1@naver.com>
2020-01-22 12:46:33 +0900

    SW2029 몫과 나머지 출력하기

--- SW_Expert_ALGORITHM/SWEA2029_D1_몫과나머지출력하기.java
+++ SW_Expert_ALGORITHM/SWEA2029_D1_몫과나머지출력하기.java
@@ -7,7 +7,7 @@ public class SWEA2029_D1_몫과나머지출력하기 {
 		for (int tc = 1; tc <= T; tc++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			System.out.println("#"+ tc + " " + a/b + " " + a%b);
+			System.out.println("#" + tc + " " + a/b + " " + a%b);
 		}
 	}
 }

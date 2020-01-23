8b5c1df263c6d33702b2850fdf57664b95d8d7c3
Hyojin Lim <qndnd1@naver.com>
2020-01-23 12:40:57 +0900

    JO562 배열1-자가진단8

---
+++ JUNGOL/Main_562.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main_562 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		for (int i = 0; i < 10; i++) {
+			arr[i] = sc.nextInt();
+		}
+		// Â¦¼öÇÕ È¦¼öÆò±Õ
+		int even_sum = 0;
+		int odd_sum = 0;
+		for (int i = 0; i < 10; i++) {
+			if ((i + 1) % 2 == 0) {
+				even_sum += arr[i];
+			}
+			else {
+				odd_sum += arr[i];
+			}
+		}
+		System.out.println("sum : " + even_sum);
+		System.out.println("avg : " + Math.round((float)odd_sum/5*10)/10.0);
+	}
+}

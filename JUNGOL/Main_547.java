dd0a4794c79ec5e75a604006849ec39b57e594ad
Hyojin Lim <qndnd1@naver.com>
2020-01-23 12:34:25 +0900

    JO547 반복제어문2-자가진단7

---
+++ JUNGOL/Main_547.java
@@ -0,0 +1,11 @@
+
+public class Main_547 {
+	public static void main(String[] args) {
+		for (int i = 2; i <= 6; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(i+j+" ");
+			}
+			System.out.println();
+		}
+	}
+}

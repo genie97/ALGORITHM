f8d6f5333bd83f04b41c68a46110fe496b257ceb
Genie <qndnd1@naver.com>
2020-01-30 21:17:14 +0900

    JO550 반복제어문3-자가진단3

---
+++ JUNGOL/Main_550.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main_550 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			for (int j = N; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}

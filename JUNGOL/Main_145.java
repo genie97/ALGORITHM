715dce7c93445730199d5639a23b9319949cdeb9
Genie <qndnd1@naver.com>
2020-01-30 22:42:01 +0900

    JO145 반복제어문3-형성평가6

---
+++ JUNGOL/Main_145.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main_145 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n-i; j++) {
+				System.out.print("  ");
+			}
+			for (int j = n-i+1; j <= n; j++) {
+				System.out.print(num++ + " ");
+			}
+			num=1;
+			System.out.println();
+		}
+
+	}
+
+}

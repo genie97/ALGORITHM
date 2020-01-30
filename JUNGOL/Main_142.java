f324410a21b42b4402497b8685c9d4a0b98a10ad
Genie <qndnd1@naver.com>
2020-01-30 22:16:12 +0900

    JO142 반복제어문3-형성평가3

---
+++ JUNGOL/Main_142.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main_142 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = n-1; i > 0; i--) {
+			for (int j = 0; j < i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}

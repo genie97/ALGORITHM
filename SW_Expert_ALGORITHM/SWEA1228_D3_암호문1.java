94aec17916d7eacf9875fbcf600ff65d3212fadd
Genie <qndnd1@naver.com>
2020-02-04 16:49:57 +0900

    SW1228 암호문1

--- SW_Expert_ALGORITHM/SWEA1228_D3_암호문1.java
+++ SW_Expert_ALGORITHM/SWEA1228_D3_암호문1.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class SWEA_1228_D3_암호문1 {
+public class SWEA1228_D3_암호문1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);

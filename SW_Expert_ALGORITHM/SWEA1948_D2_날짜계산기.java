86cefca3b2452518593599b4a343335fe7176c0e
Genie <qndnd1@naver.com>
2020-02-05 01:03:37 +0900

    SW1948 날짜계산기

---
+++ SW_Expert_ALGORITHM/SWEA1948_D2_날짜계산기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SWEA1948_D2_날짜계산기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] date = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int sm = sc.nextInt();
+			int sd = sc.nextInt();
+			int em = sc.nextInt();
+			int ed = sc.nextInt();
+
+			int sday = date[sm] - sd + 1; // 27
+			int daySum = sday;
+			for (int i = sm + 1; i < em; i++) {
+				daySum += date[i]; // 30 31
+			}
+			if (sm != em)
+				daySum += ed;
+			System.out.println("#" + tc + " " + daySum);
+			// 27 30 31 (87
+			// 15
+
+		}
+	}
+}

ed47c69167662e1b7af584222423cc2a386dd686
Genie <qndnd1@naver.com>
2020-02-04 17:17:53 +0900

    SW1928 Base64 Decoder

---
+++ SW_Expert_ALGORITHM/SWEA1928_D2_Base64Decoder.java
@@ -0,0 +1,17 @@
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.Base64.Decoder;
+
+public class SWEA1928_D2_Base64Decoder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= 10; tc++) {
+			String line = sc.next();
+			Decoder decoder = Base64.getDecoder();
+			String answer = new String(decoder.decode(line.getBytes()));
+			System.out.println("#" + tc + " " + answer);
+		}
+	}
+}

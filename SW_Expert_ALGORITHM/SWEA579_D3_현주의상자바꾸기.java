d6658c4ac74a1df7cee179117457b74bbbfa96aa
Genie <qndnd1@naver.com>
2020-02-05 15:00:39 +0900

    SW579 현주의 상자 바꾸기

---
+++ SW_Expert_ALGORITHM/SWEA579_D3_현주의상자바꾸기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SWEA579_D3_현주의상자바꾸기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int Q = sc.nextInt();
+			int[] box = new int[N + 1];
+
+			for (int i = 1; i <= Q; i++) {
+				int L = sc.nextInt();
+				int R = sc.nextInt();
+				for (int j = L; j <= R; j++) {
+					box[j] = i;
+				}
+			}
+			System.out.print("#" + tc + " ");
+			for (int i = 1; i <= N; i++) {
+				System.out.print(box[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}

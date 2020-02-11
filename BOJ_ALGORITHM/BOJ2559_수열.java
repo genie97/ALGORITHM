f099eda70feba279697039acfd5bb66c8750487a
Genie <qndnd1@naver.com>
2020-02-11 22:44:41 +0900

    BOJ2559 수열

--- BOJ_ALGORITHM/BOJ2559_수열.java
+++ BOJ_ALGORITHM/BOJ2559_수열.java
@@ -1,42 +1,24 @@
 import java.util.Scanner;
 
-public class BOJ2559_수열{
-
-	public static int K;
-	public static int N;
-	public static int max;
-	public static int[] temp;
-	public static boolean[] visit;
+public class BOJ2559_수열 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-		temp = new int[N];
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] temp = new int[N];
 
 		for (int i = 0; i < temp.length; i++) {
 			temp[i] = sc.nextInt();
 		}
-		max = Integer.MIN_VALUE;
-		visit = new boolean[N];
-		DFS(0, 0);
-		System.out.println(max);
-	}
-
-	public static void DFS(int cnt, int sum) {
-		if (cnt == K) {
-			if (sum > max) {
-				max = sum;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i <= temp.length - K; i++) {
+			int sum = 0;
+			for (int j = i; j < i + K; j++) {
+				sum += temp[j];
 			}
+			max = Integer.max(sum, max);
 		}
-		for (int i = 0; i < temp.length; i++) {
-			if (visit[i])
-				continue;
-			visit[i] = true;
-			sum += temp[i];
-			DFS(cnt + 1, sum);
-			visit[i] = false;
-			sum -= temp[i];
-		}
+		System.out.println(max);
 	}
 }

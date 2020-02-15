4f23bc42c3087109db43bd33dfa16d99fa792cad
Genie <qndnd1@naver.com>
2020-02-15 22:27:44 +0900

    SW2047 신문 헤드라인

--- SW_Expert_ALGORITHM/SWEA2047_D1_신문헤드라인.java
+++ SW_Expert_ALGORITHM/SWEA2047_D1_신문헤드라인.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SWEA_2047_D1_신문헤드라인 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String line = sc.next();
-		System.out.println(line.toUpperCase());
+		String str = sc.next();
+		System.out.println(str.toUpperCase());
 	}
 }

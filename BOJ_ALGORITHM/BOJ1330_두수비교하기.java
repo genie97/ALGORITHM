ddf515efcc1214e68e8b905a7810944ec61500cb
Genie <qndnd1@naver.com>
2020-02-23 22:20:13 +0900

    BOJ1330 두 수 비교하기

---
+++ BOJ_ALGORITHM/BOJ1330_두수비교하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1330_두수비교하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		if (A > B) {
+			System.out.println(">");
+		} else if (A < B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+}

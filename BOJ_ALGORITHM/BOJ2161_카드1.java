dc0529752fa9264317ab2c6b9a8d6a3c6c6d2c5f
Genie <qndnd1@naver.com>
2020-02-24 17:39:10 +0900

    BOJ2161 카드1

--- BOJ_ALGORITHM/BOJ2161_카드1.java
+++ BOJ_ALGORITHM/BOJ2161_카드1.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BOJ2161_카드1 {
 	public static void main(String[] args) throws IOException {

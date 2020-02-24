8953424479df86e9fe5743f516987bc269be3730
Genie <qndnd1@naver.com>
2020-02-24 19:59:23 +0900

    BOJ2443 별 찍기-6

--- BOJ_ALGORITHM/BOJ2443_별찍기_6.java
+++ BOJ_ALGORITHM/BOJ2443_별찍기_6.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class BOJ2443_별찍기_6 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

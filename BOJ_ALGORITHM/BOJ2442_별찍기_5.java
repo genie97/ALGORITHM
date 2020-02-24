4e772d09d5fa518a1f6526422e16568b5461d3f2
Genie <qndnd1@naver.com>
2020-02-24 18:31:16 +0900

    BOJ2442 별찍기-5

---
+++ BOJ_ALGORITHM/BOJ2442_별찍기_5.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2442_별찍기_5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = N - 1; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j < 2 * N - 1 - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

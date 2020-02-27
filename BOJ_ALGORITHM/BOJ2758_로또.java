ae1b6d621bb831508dd134edecdb13566c5447d0
Genie <qndnd1@naver.com>
2020-02-27 23:06:43 +0900

    BOJ2758 로또
    구현의 포인트
    -점화식 찾는 것이 어려웠던 문제
    -뽑을 장수 : n / 뽑을 수 있는 숫자 : m 일 경우, m-1개 중에서 n개를 뽑고, m/2 중에서 n-1을 뽑는다

---
+++ BOJ_ALGORITHM/BOJ2758_로또.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2758_로또 {
+
+	public static long[][] dp;
+	public static long ans;
+	public static int N;
+	public static int M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			ans = 0;
+			dp = new long[N + 1][M + 1]; // M개 중 N개 뽑기
+			dp[1][1] = 1;
+
+			// i개 중에 1개 뽑는 경우는 i가지
+			for (int i = 1; i <= M; i++) {
+				dp[1][i] = i;
+			}
+			
+			for (int i = 2; i <= N; i++) {
+				for (int j = 1; j <= M; j++) {
+					if (i > j)
+						continue;
+					dp[i][j] = dp[i][j - 1] + dp[i - 1][j / 2];
+				}
+			}
+			System.out.println(dp[N][M]);
+		}
+	}
+}

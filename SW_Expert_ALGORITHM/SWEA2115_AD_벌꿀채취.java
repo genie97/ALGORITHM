d61a0a7b973dbe96544e7de3e3b6a997c1c3f96e
Genie <qndnd1@naver.com>
2020-02-28 23:01:02 +0900

    SW2115 벌꿀채취
    구현 포인트
    - 일꾼 2명이 갈 꿀통 조합 =>  M만큼 연속된 값은 선택할 수 없는 것, 가로로만 조합이 가능하다는 것으로 가지치기 가능
    - 채집제한량을 넘으면 모든 양을 수집하지 못할 수도 있으므로 확인해서 가지치기 가능

---
+++ SW_Expert_ALGORITHM/SWEA2115_AD_벌꿀채취.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class SWEA2115_AD_벌꿀채취 {
+	public static int[][] honeyMap;
+	public static ArrayList<int[]> list;
+	public static int N;
+	public static int M;
+	public static int C;
+	public static int remain;
+	public static int powS;
+	public static int benefit;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			honeyMap = new int[N][N];
+			list = new ArrayList<int[]>();
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					honeyMap[i][j] = Integer.parseInt(st.nextToken());
+					list.add(new int[] { i, j });
+				}
+			}
+
+			benefit = Integer.MIN_VALUE;
+
+			// 일꾼 두명이 공간 2개 선택
+			for (int i = 0; i < list.size() - 2; i++) {
+				int x1 = list.get(i)[0];
+				int y1 = list.get(i)[1];
+				if (y1 + (M - 1) >= N)
+					continue;
+				for (int j = i + 1; j < list.size() - 1; j++) {
+					int x2 = list.get(j)[0];
+					int y2 = list.get(j)[1];
+					if (x1 == x2) {
+						if (y1 + (M - 1) >= y2)
+							continue;
+					}
+					if (y2 + (M - 1) >= N)
+						continue;
+//					System.out.println("[" + x1 + ", " + y1 + "]" + "[" + x2 + ", " + y2 + "]");
+					int value = maxBenefit(x1, y1, x2, y2);
+//					System.out.println(value);
+					if (value > benefit) {
+						benefit = value;
+					}
+				}
+			}
+			System.out.println("#" + tc + " " + benefit);
+		}
+	}
+
+	public static int maxBenefit(int x1, int y1, int x2, int y2) {
+		// 일꾼 1 -> 일꾼 2
+		powS = 0;
+		getHoney(x1, y1, 0, 0, powS); // 위치 / cnt / sum /powSum
+		getHoney(x2, y2, 0, 0, powS);
+		int b1 = powS;
+
+		// 일꾼 2 -> 일꾼 1
+		powS = 0;
+		getHoney(x2, y2, 0, 0, powS); // 위치 / cnt / sum /powSum
+		getHoney(x1, y1, 0, 0, powS);
+		int b2 = powS;
+
+		return Math.max(b1, b2);
+
+	}
+
+	public static void getHoney(int x, int y, int cnt, int sum, int powSum) {
+		if (sum > C)
+			return;
+		if (cnt == M) {
+			if (powS < powSum) {
+				powS = powSum; // 이득 저장
+			}
+			return;
+		}
+		getHoney(x, y + 1, cnt + 1, sum + honeyMap[x][y], powSum + (honeyMap[x][y] * honeyMap[x][y]));
+		getHoney(x, y + 1, cnt + 1, sum, powSum);
+
+	}
+}

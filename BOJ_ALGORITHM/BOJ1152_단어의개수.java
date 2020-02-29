dfed71d47d28740be7ef2819f5ff0af69e3b3a12
Genie <qndnd1@naver.com>
2020-02-29 19:26:47 +0900

    BOJ1152 단어의 개수
    포인트 메서드
    StringTokenizer의 hasMoreTokens( ) => 반환할 토큰이 남아있는지 확인하는 메서드

---
+++ BOJ_ALGORITHM/BOJ1152_단어의개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1152_단어의개수 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int cnt = 0;
+		while (st.hasMoreTokens()) {
+			st.nextToken();
+			cnt++;
+		}
+		System.out.println(cnt);
+
+	}
+
+}

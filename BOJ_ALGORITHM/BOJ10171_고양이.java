0c57ec38a858e19ad6582d1ede6419efce2a9d86
Genie <qndnd1@naver.com>
2020-02-29 20:07:10 +0900

    BOJ10171 고양이
    구현 시 주의 사항
    - 출력 텍스트 그대로 복사하면 \r값이 들어가서 주의해야한다

---
+++ BOJ_ALGORITHM/BOJ10171_고양이.java
@@ -0,0 +1,8 @@
+public class BOJ10171_고양이 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|");
+		System.out.println(sb);
+	}
+}

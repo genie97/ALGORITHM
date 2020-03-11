5adc43a698c6d03310dbf94e8ae8512a23845a75
Genie <qndnd1@naver.com>
2020-03-11 22:03:39 +0900

    SW7701 염라대왕의 이름 정렬

---
+++ SW_Expert_ALGORITHM/SWEA7701_D4_염라대왕의이름정렬.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class SWEA7701_D4_염라대왕의이름정렬 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			HashSet<String> s = new HashSet<>();
+			for (int i = 0; i < N; i++) {
+				String n = br.readLine();
+				s.add(n);
+			}
+			ArrayList<String> list = new ArrayList<>(s);
+			Collections.sort(list, new Comparator<String>() {
+				@Override
+				public int compare(String o1, String o2) {
+					if (o1.length() < o2.length())
+						return -1;
+					else {
+						if (o1.length() == o2.length())
+							return o1.compareTo(o2);
+						else
+							return 1;
+					}
+				}
+			});
+			sb.append("#").append(tc).append("\n");
+			for (int i = 0; i < list.size(); i++) {
+				sb.append(list.get(i)).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}

5d100d6720ecbfded9bb9f31b1599f550192e316
HYOJIN LIM <qndnd1@naver.com>
2020-04-10 13:00:49 +0900

    BOJ9466 텀 프로젝트

--- BOJ_ALGORITHM/BOJ9466_텀프로젝트.java
+++ BOJ_ALGORITHM/BOJ9466_텀프로젝트.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ9466_텀프로젝트 {
+public class BOJ9466_텀프로젝트_Main_임효진 {
 	static int N;
 	static int[] student;
 	static boolean[] start;
@@ -19,15 +19,15 @@ public class BOJ9466_텀프로젝트 {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 			student = new int[N + 1];
-			start = new boolean[N + 1];
-			finish = new boolean[N + 1];
+			start = new boolean[N + 1]; // 처음 시작할 때 선택
+			finish = new boolean[N + 1]; // 선택하고 다시 돌아온 경우 끝났다로 표시
 			cnt = 0;
 
 			for (int i = 1; i <= N; i++) {
 				student[i] = Integer.parseInt(st.nextToken());
 			}
 			for (int i = 1; i <= N; i++) {
-				if (!start[i] && !finish[i])
+				if (!start[i] && !finish[i]) // 시작도 안하고, 끝도 안난 정점으로만 dfs
 					dfs(i);
 			}
 			sb.append(N - cnt).append('\n');
@@ -37,16 +37,16 @@ public class BOJ9466_텀프로젝트 {
 
 	static void dfs(int v) {
 		start[v] = true;
-		int nv = student[v];
-		if (!start[nv])
+		int nv = student[v]; // 다음 정점
+		if (!start[nv]) // 첫 시작 방문을 안했다면 dfs
 			dfs(nv);
-		else {
-			if (!finish[nv]) {
+		else { // 시작 방문을 했을 때,
+			if (!finish[nv]) { // 끝이 나지 않았다면 사이클이니 확인해주기
 				for (int vt = nv; v != vt; vt = student[vt])
-					cnt++;
+					cnt++; 
 				cnt++;
 			}
 		}
-		finish[v] = true;
+		finish[v] = true; // 최종적으로 v는 사이클확인까지 끝났다라고 표시
 	}
 }

a48782ca61e6dd96803e1d9ae466b7b6a199bf76
HYOJIN LIM <qndnd1@naver.com>
2020-04-23 03:26:34 +0900

    BOJ11066 파일 합치기

--- BOJ_ALGORITHM/BOJ11066_파일합치기.java
+++ BOJ_ALGORITHM/BOJ11066_파일합치기.java
@@ -4,33 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ11066_파일합치기 {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
+		for (int tc = 0; tc < T; tc++) {
 			int N = Integer.parseInt(br.readLine());
 			int[] sum = new int[N + 1];
-			int[][] dp = new int[N + 1][N + 1];
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
-			// 누적합
+			// Ex) N = 4
+			// Ex) 40 30 30 50
+			// 누적합 40 70 100 150
 			for (int i = 1; i <= N; i++) {
-				sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+				sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
 			}
-			
-			
-			for (int l = 2; l <= N; l++) { // 압축하려고 하는 길이 (2~N개까지 존재)
-				for (int i = 1; i <= N - l + 1; i++) {
-					int j = i + l - 1;
-					dp[i][j] = Integer.MAX_VALUE;
+			int[][] chain = new int[N + 1][N + 1];
+			for (int len = 2; len <= N; len++) { // 합치는 길이 (연속 2~N까지)
+				for (int i = 1; i <= N - len + 1; i++) { // 1 ~ 길이만큼 확인 (len=2일때, [1,2] / [2,3] / [3,4]
+					int j = i + len - 1;
+					chain[i][j] = Integer.MAX_VALUE;
 
 					for (int k = i; k < j; k++) {
-						int c = dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1];
-						dp[i][j] = Math.min(dp[i][j], c);
+						chain[i][j] = Math.min(chain[i][j], chain[i][k] + chain[k + 1][j] + sum[j] - sum[i - 1]);
 					}
 				}
 			}
-			System.out.println(dp[1][N]);
+			sb.append(chain[1][N]).append('\n'); // 1~N까지의 길이
 		}
+		System.out.println(sb);
 	}
+
 }

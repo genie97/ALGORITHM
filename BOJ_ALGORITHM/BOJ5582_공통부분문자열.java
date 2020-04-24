78cb5893f61f12e9a68e9ddfe763193ca0076936
HYOJIN LIM <qndnd1@naver.com>
2020-04-24 19:34:49 +0900

    BOJ5582 공통 부분 문자열

--- BOJ_ALGORITHM/BOJ5582_공통부분문자열.java
+++ BOJ_ALGORITHM/BOJ5582_공통부분문자열.java
@@ -25,3 +25,37 @@ public class BOJ5582_공통부분문자열 {
 		System.out.println(max);
 	}
 }
+
+/* 200ms
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ5582_공통부분문자열 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str1 = br.readLine();
+		String str2 = br.readLine();
+		if (str1.length() > str2.length()) {
+			String tmp = str1;
+			str1 = str2;
+			str2 = tmp;
+		}
+		int[][] LCS = new int[str1.length() + 1][str2.length() + 1];
+		int ans = 0;
+		for (int i = 1; i < LCS.length; i++) {
+			for (int j = 1; j < LCS[i].length; j++) {
+				if (str1.charAt(i - 1) == str2.charAt(j - 1))  // 같다면 대각선에서 가져옴
+					LCS[i][j] = LCS[i - 1][j - 1] + 1;
+				ans = Math.max(ans, LCS[i][j]);
+			}
+		}
+//		for (int i = 0; i < LCS.length; i++) {
+//			System.out.println(Arrays.toString(LCS[i]));
+//		}
+		System.out.println(ans);
+	}
+}
+
+*/

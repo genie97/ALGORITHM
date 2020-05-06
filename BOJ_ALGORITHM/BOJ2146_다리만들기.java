aa5afa2b0b98bbfaa726f2dfdc82d1360add9a03
HYOJIN LIM <qndnd1@naver.com>
2020-05-07 00:01:02 +0900

    BOJ2146 다리 만들기

--- BOJ_ALGORITHM/BOJ2146_다리만들기.java
+++ BOJ_ALGORITHM/BOJ2146_다리만들기.java
@@ -1,3 +1,99 @@
+/* 196ms */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ2146_다리만들기 {
+	static int N;
+	static int[][] map;
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static int res;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+
+		// 육지를 0, 바다는 -1로 세팅
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0, idx = 0; j < N; j++, idx += 2) {
+				map[i][j] = (str.charAt(idx) == '1') ? 0 : -1;
+			}
+		}
+
+		//섬 번호를 1번부터 매긴다
+		int idx = 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == 0) {
+					map[i][j] = idx;
+					numbering(i, j, idx++);
+				}
+			}
+		}
+
+		res = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] > 0) {
+					makeBridge(i, j);
+				}
+			}
+		}
+		System.out.println(res);
+	}
+
+	static void makeBridge(int x, int y) {
+		Queue<int[]> q = new LinkedList<>();
+		boolean[][] visit = new boolean[N][N];
+		q.add(new int[] { x, y, 0 });
+		visit[x][y] = true;
+		int base = map[x][y];
+
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			if(cur[2] >= res) break;
+			for (int d = 0; d < 4; d++) {
+				int nx = cur[0] + dx[d];
+				int ny = cur[1] + dy[d];
+				if (isIn(nx, ny)) { // 맵 안에 있는 경우
+					if (visit[nx][ny] || map[nx][ny] == base) // 방문했거나 같은 섬인 경우는 제외
+						continue;
+					if (map[nx][ny] == -1) {// 바다인 경우
+						q.add(new int[] { nx, ny, cur[2] + 1 });
+						visit[nx][ny] = true;
+					} else if (map[nx][ny] > 0 && map[nx][ny] != base) { // 다른 땅 도착인 경우
+						res = Math.min(res, cur[2]);
+					}
+				}
+			}
+		}
+	}
+
+	static void numbering(int x, int y, int idx) {
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if (!isIn(nx, ny))
+				continue;
+			if (map[nx][ny] == -1 || map[nx][ny] == idx)
+				continue;
+			map[nx][ny] = idx;
+			numbering(nx, ny, idx);
+		}
+	}
+
+	static boolean isIn(int x, int y) {
+		return 0 <= x && 0 <= y && x < N && y < N;
+	}
+
+}
+
+/* 256ms
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -106,3 +202,4 @@ public class BOJ2146_다리만들기 {
 	}
 
 }
+*/

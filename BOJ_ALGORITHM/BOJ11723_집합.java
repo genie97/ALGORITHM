fc359022076f60bed192a9f9099ff2508232a2fe
HYOJIN LIM <qndnd1@naver.com>
2020-05-22 00:14:24 +0900

    BOJ11723 집합

--- BOJ_ALGORITHM/BOJ11723_집합.java
+++ BOJ_ALGORITHM/BOJ11723_집합.java
@@ -1,3 +1,58 @@
+/*비트연산 방식*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//S는 집합
+// add x S에 추가
+// remove x S에서 제거 없으면 무시
+// check x x가 있으면 1 없으면 0 => 연산 결과 출력
+// toggle x x가 있으면 x제거 없으면 추가
+// all S를 {1,2,...20}으로 변경
+// empty: S를 공집합으로
+
+public class BOJ11723_집합 {
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int s = 0;
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String comm = st.nextToken();
+			int x = 0;
+			switch (comm) {
+			case "add":
+				x = Integer.parseInt(st.nextToken()) - 1;
+				s = (s | 1 << x);
+				break;
+			case "remove":
+				x = Integer.parseInt(st.nextToken()) - 1;
+				s = (s & ~(1 << x));
+				break;
+			case "check":
+				x = Integer.parseInt(st.nextToken()) - 1;
+				sb.append((s & (1 << x)) != 0 ? 1 : 0).append('\n');
+				break;
+			case "toggle":
+				x = Integer.parseInt(st.nextToken()) - 1;
+				s = s ^ (1 << x);
+				break;
+			case "all":
+				s = (1 << 20) - 1;
+				break;
+			case "empty":
+				s = 0;
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+}
+
+
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -66,3 +121,4 @@ public class BOJ11723_집합 {
 		System.out.println(sb);
 	}
 }
+*/

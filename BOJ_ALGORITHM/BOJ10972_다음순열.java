cdd0c3981e17e3fa162ce759841e74fb065f74e7
HYOJIN LIM <qndnd1@naver.com>
2020-05-27 23:41:01 +0900

    BOJ10972 다음 순열

--- BOJ_ALGORITHM/BOJ10972_다음순열.java
+++ BOJ_ALGORITHM/BOJ10972_다음순열.java
@@ -1,22 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ10972_다음순열 {
 	static int[] list;
+	static int N;
 
 	public static void main(String[] args) throws IOException {
+		StringBuilder sb = null;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		list = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		for (int i = 0; i < N; i++) {
 			list[i] = Integer.parseInt(st.nextToken());
 		}
-		
-
+		if (nextPermutation()) {
+			for (int i = 0; i < N; i++) {
+				System.out.print(list[i]+" ");
+			}
+		} else {
+			System.out.println(-1);
+		}
 	}
 
+	static boolean nextPermutation() {
+		int i = N - 1;
+		while (i > 0 && list[i - 1] >= list[i])
+			--i;
+		if (i == 0)
+			return false;
+
+		int j = N - 1;
+		while (list[i - 1] >= list[j])
+			--j;
+
+		int tmp = list[i - 1];
+		list[i - 1] = list[j];
+		list[j] = tmp;
+
+		int k = N - 1;
+		while (i < k) {
+			tmp = list[i];
+			list[i] = list[k];
+			list[k] = tmp;
+			++i;
+			--k;
+		}
+		return true;
+	}
 }

b7a1a0f99170e78af303bdf2886f2cbbf7bf0d24
HYOJIN LIM <qndnd1@naver.com>
2020-08-29 02:51:29 +0900

    BOJ2743 단어 길이 재기

---
+++ BOJ_ALGORITHM/BOJ2743_단어길이재기.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743_단어길이재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		System.out.println(str.length());
+	}
+}

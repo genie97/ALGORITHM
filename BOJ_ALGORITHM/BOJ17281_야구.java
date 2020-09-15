76d3bcdab00c824412f909ecc367935e752f41c6
HYOJIN LIM <qndnd1@naver.com>
2020-09-15 22:35:44 +0900

    BOJ17281_⚾

---
+++ BOJ_ALGORITHM/BOJ17281_야구.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ17281_야구 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), null);
+			for (int j = 0; j < 9; j++) {
+
+			}
+		}
+
+	}
+}

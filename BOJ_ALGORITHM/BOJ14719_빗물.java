dbdea9ff4831fa429c1f6a032174cdd71aa81d67
HYOJIN LIM <qndnd1@naver.com>
2020-09-24 02:06:47 +0900

    BOJ14719 빗물
    * 구현 방법 *
    - 오른쪽과 왼쪽을 양분해서 확인하면서 오른쪽 max와 왼쪽 max 높이를 찾는다
    - 오른쪽과 왼쪽 중 min값으로 빗물이 얼마나 고일 수 있는지 확인한다

---
+++ BOJ_ALGORITHM/BOJ14719_빗물.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ14719_빗물 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int[] block = new int[W];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < W; i++) {
+			block[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int sum = 0;
+
+		for (int i = 1; i < W; i++) {
+			int right = 0;
+			int left = 0;
+			for (int j = 0; j < i; j++) {
+				left = Math.max(left, block[j]);
+			}
+			for (int j = W - 1; j > i; j--) {
+				right = Math.max(right, block[j]);
+			}
+			sum += Math.max(0, Math.min(right, left) - block[i]);
+		}
+
+		System.out.println(sum);
+	}
+}

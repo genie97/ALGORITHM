00d3d2c7997d272c05b03003bd1a498638201a4e
HYOJIN LIM <qndnd1@naver.com>
2021-02-01 15:55:04 +0900

    BOJ1806 부분합

--- BOJ_ALGORITHM/BOJ1806_부분합.java
+++ BOJ_ALGORITHM/BOJ1806_부분합.java
@@ -3,10 +3,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 10000이하의 자연수로 이루어진 N짜리 수열
-// 그 합이 S이상 되는 것 중 가장 짧은 것의 길이
-// N (10 ≤ N < 100,000)과 S (0 < S ≤ 100,000,000)
-// 전형적인 슬라이딩 윈도우 (투포인터 알고리즘)
+//10000이하의 자연수로 이루어진 N짜리 수열
+//그 합이 S이상 되는 것 중 가장 짧은 것의 길이
+//N (10 ≤ N < 100,000)과 S (0 < S ≤ 100,000,000)
+//전형적인 슬라이딩 윈도우 (투포인터 알고리즘)
 
 public class BOJ1806_부분합 {
 	public static void main(String[] args) throws IOException {
@@ -33,14 +33,14 @@ public class BOJ1806_부분합 {
 			if (sum < S && end + 1 <= N) {
 				sum += arr[end];
 				end++;
-			} else if (sum >= S || end == N) 
-				// 앞쪽 포인터를 옮기기 전에 합이 S이상이면 길이 확인하기{
-				if (sum >= S) { 
+			} else if (sum >= S || end == N) {
+				// 앞쪽 포인터를 옮기기 전에 합이 S이상이면 길이 확인하기
+				if (sum >= S) {
 					ans = Math.min(ans, end - start);
 				}
 				// 밖의 조건문에서 확인할 경우, 제일 마지막을 확인 못할 수 있으므로 
 				// 안쪽에서 다시 확인하기
-				if (start + 1 <= N) { 
+				if (start + 1 <= N) {
 					sum -= arr[start];
 					start++;
 				}

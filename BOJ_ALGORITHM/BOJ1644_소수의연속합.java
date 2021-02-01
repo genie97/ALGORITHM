4d0855874fbbce36f3577d8190325c2ab48e1910
HYOJIN LIM <qndnd1@naver.com>
2021-02-01 18:10:39 +0900

    BOJ1644 소수의 연속합

---
+++ BOJ_ALGORITHM/BOJ1644_소수의연속합.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ1644_소수의연속합 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		boolean[] prime = new boolean[N + 1]; // 소수 확인용
+
+		prime[1] = true;
+
+		// 1. 소수와 합성수를 구분한다
+		for (int i = 2; i <= (int) (Math.sqrt(N)); i++) {
+			if (prime[i])
+				continue;
+			for (int j = i + i; j <= N; j += i) {
+				prime[j] = true;
+			}
+		}
+		
+		// 2. 소수 리스트를 확인한다
+		ArrayList<Integer> prime_list = new ArrayList<>();
+		for (int i = 2; i <= N; i++) {
+			if (prime[i])
+				continue;
+			prime_list.add(i);
+		}
+
+		// 3. 슬라이딩 윈도우로 합이 N이 되는 경우를 찾는다
+		int start = 0;
+		int end = 0;
+		int sum = 0;
+		int ans = 0;
+		while (true) {
+			if (start == end && end == prime_list.size())
+				break;
+
+			if (sum < N && end + 1 <= prime_list.size()) {
+				sum += prime_list.get(end);
+				end++;
+			} else if (sum >= N || start + 1 <= prime_list.size()) {
+				if (sum == N) {
+					ans++;
+				}
+				if (start + 1 <= prime_list.size()) {
+					sum -= prime_list.get(start);
+					start++;
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}

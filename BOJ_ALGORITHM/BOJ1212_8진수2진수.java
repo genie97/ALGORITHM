d2d7ce94188998c4a1e3eaff6308159ac6a41ccb
HYOJIN LIM <qndnd1@naver.com>
2021-02-04 17:50:41 +0900

    BOJ1212 8진수 2진수

--- BOJ_ALGORITHM/BOJ1212_8진수2진수.java
+++ BOJ_ALGORITHM/BOJ1212_8진수2진수.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ1373_8진수2진수 {
+public class BOJ1212_8진수2진수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();

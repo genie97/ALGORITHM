139da65d93a19253c50cd149fb92ed8ba3b54690
HYOJIN LIM <qndnd1@naver.com>
2021-03-10 15:28:28 +0900

    SW2043 서랍의 비밀번호

--- SW_Expert_ALGORITHM/SWEA2043_D1_서랍의비밀번호.java
+++ SW_Expert_ALGORITHM/SWEA2043_D1_서랍의비밀번호.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SWEA2042_D1_서랍의비밀번호 {
+public class SWEA2043_D1_서랍의비밀번호 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int p = sc.nextInt();
